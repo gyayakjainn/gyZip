bdc58567a198773a34e55d9500a44f9b8915837e
gyayakjainn <gyayakjain01@gmail.com>
2025-07-13 23:32:09 +0530

    Update run.sh
    added new compile time flags

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-g++ -std=c++17 -UENABLE_LOGS -DSHOW_TREE -O2 -o compress compress.cpp
+g++ -std=c++17 -DENABLE_LOGS -USHOW_TREE -USHOW_BYTES -O2 -o compress compress.cpp
 .\compress.exe .\input.txt
 
-g++ -std=c++17 -UENABLE_LOGS -DSHOW_TREE -O2 -o decompress decompress.cpp
+g++ -std=c++17 -UENABLE_LOGS -DSHOW_TREE -DSHOW_BYTES -O2 -o decompress decompress.cpp
 .\decompress.exe .\input.txt.bin
